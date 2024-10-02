4cb56b13ea35a63977bbf20c767c415b03044523
赵国平 <zhaoguoping@jylink.com>
2024-10-02 22:05:22 +0800

    Add pointer

--- junior/static_const.cpp
+++ junior/static_const.cpp
@@ -10,8 +10,13 @@ void staticVariable() {
 
 class StaticConst {
     public: 
-        static const int maxInstances = 2;
-        static int instanceCount;
+        static const int maxInstances = 2;  // 类常量，可以在类中初始化
+        static int instanceCount;  // 类变量，不能在类中初始化
+
+        static void display(){
+            cout << "调用类方法" << endl;
+            cout << "当前实例个数是：" << instanceCount << endl;
+        };
     
     StaticConst() {
         if (instanceCount < maxInstances) {
@@ -21,14 +26,18 @@ class StaticConst {
             cout << "已达到能创建实例的最大数量！！！" << endl;
         }
     }
+
+
 };
 
-int StaticConst::instanceCount = 0;
+int StaticConst::instanceCount = 0;  // 全局初始化类变量
 
 int main() {
-    for (int i = 0; i < 3; i++) {
-        StaticConst sc;
-    }
+    int x = 100;
+    const int* ptr = &x;
+    cout << *ptr << endl;
+    cout << ptr << endl;
+    cout << "信息学奥林匹克" << endl;
 
     return 0;
 }
