ae42846450653a94bb010ec4720f4a29c7649417
赵国平 <zhaoguoping@jylink.com>
2024-10-07 22:08:55 +0800

    Dynamic program start

---
+++ src/common/dynamic_program.cpp
@@ -0,0 +1,6 @@
+#include "dynamic_program.h"
+
+
+int dp_minium_coins(int m, vector<int>& coins) {
+    
+}
---
+++ src/common/dynamic_program.h
@@ -0,0 +1,9 @@
+/**
+ * 动态规划，硬币问题
+ */
+#include<iostream>
+
+using namespace std;
+
+
+int dp_minium_coins(int m, vector<int>& coins);
